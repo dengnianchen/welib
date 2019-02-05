bc27cf88b66c6d6150f795f1c0f134185806dccd
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-02-05 12:37:15 +0800

    $.String添加函数lowerDashToCamel

--- string.js
+++ string.js
@@ -4,7 +4,7 @@ class _String {
 	 * 将驼峰形式字符串转换为小写横杠形式字符串
 	 *
 	 * @param str 驼峰形式字符串
-	 * @return {String} 小写横杠形式字符串
+	 * @return {string} 小写横杠形式字符串
 	 * @author Deng Nianchen
 	 */
 	static camelTolowerDash(str) {
@@ -12,6 +12,19 @@ class _String {
 		if (temp.slice(0, 1) === '_')
 			temp = temp.slice(1);
 		return temp;
+	}
+	
+	/**
+	 * 将小写横杠形式字符串转换为驼峰形式字符串
+	 *
+	 * @param str 小写横杠形式字符串
+	 * @returns {string} 驼峰形式字符串
+	 * @author Deng Nianchen
+	 */
+	static lowerDashToCamel(str) {
+		return str.replace(/([^-])(?:-+([^-]))/g, function ($0, $1, $2) {
+			return $1 + $2.toUpperCase();
+		});
 	}
 	
 }
