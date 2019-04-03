1c47e5bc11da1258c68e2629089929cfe38a2676
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-04-03 16:35:14 +0800

    版本1.10.6：App扩展：修复检查更新导致持续显示加载中的问题

--- extension/app.js
+++ extension/app.js
@@ -3,30 +3,14 @@
 	App = function(app) {
 		app.initializeResolves = [];
 		
-		app.checkUpdate = async function () {
-			return new Promise(resolve => {
-				let updateManager = wx.getUpdateManager();
-				updateManager.onCheckForUpdate(result => {
-					if (!result.hasUpdate)
-						resolve();
-					else
-						Page.current().setLoading("发现更新，正在下载");
-				});
-				updateManager.onUpdateFailed(async () => {
-					await $.Modal.show('版本更新',
-						'更新下载失败。非最新版本的小程序可能无法正常运行。重启小程序可重新尝试更新。', {
-							showCancel: false
-						});
-					resolve();
-				});
-				updateManager.onUpdateReady(async () => {
-					await $.Modal.show('版本更新',
-						'更新已下载，点击确认完成更新并重启小程序。', {
-						showCancel: false
-					});
-					Page.current().setLoading("正在安装更新");
+		app.checkUpdate = function () {
+			let updateManager = wx.getUpdateManager();
+			updateManager.onUpdateReady(async function () {
+				let res = await $.Modal.show('更新提示', '新版本已经准备好，是否重启以完成更新？');
+				if (res.confirm) {
+					// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
 					updateManager.applyUpdate();
-				});
+				}
 			});
 		};
 		
@@ -44,8 +28,8 @@
 			$.initial(this);
 			try {
 				this.initializing = true;
-				// 1. 检查更新
-				await this.checkUpdate();
+				// 1. 异步检查更新
+				this.checkUpdate();
 				// 2. 调用onLaunch函数进行用户自定义初始化（若存在）
 				if (app._onLaunch instanceof Function)
 					await app._onLaunch(option);
